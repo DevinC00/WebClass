c1e8dc9c9094daf618c41bf14f4857ae2a07890f
DevinC00 <dcohen15@students.kennesaw.edu>
2026-05-03 22:06:06 -0400

    change

--- burger.js
+++ burger.js
@@ -1,5 +1,5 @@
 const burger = document.getElementById('.burger');
-const nav = document.getElementById('.nav-links');
+const nav = document.getElementById('.navi-links');
 
 burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
